60ecfd26306b8dc5db32567eecc5d221840f5737
Thanhhn23 <thanhhn@antsomi.com>
2023-08-08 12:14:59 +0700

    add-event-purchase

--- src/component/EditProduct.js
+++ src/component/EditProduct.js
@@ -114,6 +114,15 @@ function EditProduct({ id, closeModal }) {
                 })
             })
 
+            web_event.track("product", "purchase", {
+                items:  [{type: "product", parent_item_id : "1", ...data[0]}],
+                    dims: {},
+                    extra: {
+                        title: "sac du phong",                       
+                        keywords: "wifi"
+                    }
+            })
+
     }, [])
 
     return (
