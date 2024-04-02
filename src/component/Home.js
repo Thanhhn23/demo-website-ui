514588ac8b0a138223e9bf6e06f937154ccc1f8a
Thanhhn23 <thanhhn@antsomi.com>
2024-04-02 12:07:42 +0700

    vv

--- src/component/Home.js
+++ src/component/Home.js
@@ -215,7 +215,7 @@ function handleCloseAddProduct() {
         </div>
           
 
-     <antsomi-webinbox portalId="561236459" destinationId="8277302" lookupType="visitor" lookupId={visitorId} selector="body" />
+     <antsomi-webinbox portalId="33167" destinationId="1350042" lookupType="visitor" lookupId={visitorId} selector="body" />
 
       </div>
          
